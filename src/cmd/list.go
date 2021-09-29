4812d182833627996a19929848b6467888f6cb24
Kyle Rockman <rocktavious@icloud.com>
2021-09-29 12:57:49 -0500

    feature: add ability to output list data as a json array

--- src/cmd/list.go
+++ src/cmd/list.go
@@ -2,14 +2,24 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var listOutputType string
 
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "List resources or data from OpsLevel",
-	Long:  "List resources or data from OpsLevel",
+	Short: "List all resources in OpsLevel",
+	Long:  "List all resources in OpsLevel",
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.PersistentFlags().StringVarP(&listOutputType, "output", "o", "text", "Output format.  One of: json|text [default: text]")
+	viper.BindPFlags(createCmd.Flags())
+}
+
+func isJsonOutput() bool {
+	return listOutputType == "json"
 }
