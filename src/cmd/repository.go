afe930ee21a542f273f613d6384cfa0320d9aa4e
Kyle Rockman <rocktavious@icloud.com>
2021-09-29 12:58:14 -0500

    feature: implement get & list for repository

---
+++ src/cmd/repository.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/opslevel/cli/common"
+	"github.com/opslevel/opslevel-go"
+	"github.com/spf13/cobra"
+)
+
+var getRepositoryCmd = &cobra.Command{
+	Use:        "repository ID|ALIAS",
+	Short:      "Get details about a repository",
+	Long:       `Get details about a repository`,
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID", "ALIAS"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		var repository *opslevel.Repository
+		var err error
+		if common.IsID(key) {
+			repository, err = graphqlClient.GetRepository(key)
+			cobra.CheckErr(err)
+		} else {
+			repository, err = graphqlClient.GetRepositoryWithAlias(key)
+			cobra.CheckErr(err)
+		}
+		cobra.CheckErr(err)
+		common.PrettyPrint(repository)
+	},
+}
+
+var listRepositoryCmd = &cobra.Command{
+	Use:     "repository",
+	Aliases: []string{"repositories"},
+	Short:   "Lists repositories",
+	Long:    `Lists repositories`,
+	Run: func(cmd *cobra.Command, args []string) {
+		list, err := graphqlClient.ListRepositories()
+		cobra.CheckErr(err)
+		if isJsonOutput() {
+			common.JsonPrint(json.MarshalIndent(list, "", "    "))
+		} else {
+			w := common.NewTabWriter("NAME", "ALIAS", "ID")
+			for _, item := range list {
+				fmt.Fprintf(w, "%s\t%s\t%s\t\n", item.Name, item.DefaultAlias, item.Id)
+			}
+			w.Flush()
+		}
+	},
+}
+
+func init() {
+	getCmd.AddCommand(getRepositoryCmd)
+	listCmd.AddCommand(listRepositoryCmd)
+}
