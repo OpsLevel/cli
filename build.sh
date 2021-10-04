e414df4c26c28739b0a5437b69f39219c1b565b2
Kyle Rockman <rocktavious@icloud.com>
2021-10-04 09:43:33 -0500

    bring dockerfile inline with kubectl-opslevel

--- build.sh
+++
@@ -1,4 +0,0 @@
-VERSION=$(git describe --tags --match "v[0-9].*" --always)
-CGO_ENABLED=0 go build -o ./opslevel -ldflags="-X 'github.com/opslevel/cli/cmd.version=${VERSION}'"
-chmod +x ./opslevel
-mv ./opslevel /usr/local/bin/opslevel
