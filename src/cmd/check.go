2c428454c93e7532be7fc90d1f7d2ff5fd0ba8fd
Kyle Rockman <rocktavious@icloud.com>
2021-12-10 10:59:28 -0600

    feature: add ability to create custom event checks from a yaml file

--- src/cmd/check.go
+++ src/cmd/check.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/creasty/defaults"
 	"github.com/opslevel/cli/common"
+	"github.com/opslevel/opslevel-go"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var getCheckCmd = &cobra.Command{
@@ -15,9 +18,9 @@ var getCheckCmd = &cobra.Command{
 	Args:       cobra.ExactArgs(1),
 	ArgAliases: []string{"ID"},
 	Run: func(cmd *cobra.Command, args []string) {
-		category, err := graphqlClient.GetCheck(args[0])
+		check, err := graphqlClient.GetCheck(args[0])
 		cobra.CheckErr(err)
-		common.PrettyPrint(category)
+		common.PrettyPrint(check)
 	},
 }
 
@@ -41,6 +44,32 @@ var listCheckCmd = &cobra.Command{
 	},
 }
 
+var checkCreateCmd = &cobra.Command{
+	Use:   "check",
+	Short: "Create a rubric check",
+	Long: `Create a rubric check
+
+Examples:
+
+	opslevel create check -f my_cec.yaml
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		common.AliasCache.CacheCategories(graphqlClient)
+		common.AliasCache.CacheLevels(graphqlClient)
+		common.AliasCache.CacheTeams(graphqlClient)
+		common.AliasCache.CacheFilters(graphqlClient)
+		common.AliasCache.CacheIntegrations(graphqlClient)
+		input, err := readCheckCreateInput()
+		cobra.CheckErr(err)
+		switch input.Kind {
+		case opslevel.CheckTypeCustom:
+			check, err := graphqlClient.CreateCheckCustomEvent(*input.AsCustomEventCreate())
+			cobra.CheckErr(err)
+			fmt.Printf("Created: %s - %s\n", check.Name, check.Id)
+		}
+	},
+}
+
 var deleteCheckCmd = &cobra.Command{
 	Use:        "check ID",
 	Short:      "Delete a rubric check",
@@ -56,7 +85,66 @@ var deleteCheckCmd = &cobra.Command{
 }
 
 func init() {
+	createCmd.AddCommand(checkCreateCmd)
 	getCmd.AddCommand(getCheckCmd)
 	listCmd.AddCommand(listCheckCmd)
 	deleteCmd.AddCommand(deleteCheckCmd)
+}
+
+type CheckCreateType struct {
+	Kind opslevel.CheckType
+	Spec map[string]interface{}
+}
+
+func (self *CheckCreateType) resolveAliases() {
+	if item, ok := self.Spec["category"]; ok {
+		if value, ok := common.AliasCache.TryGetCategory(item.(string)); ok {
+			delete(self.Spec, "category")
+			self.Spec["categoryId"] = value.Id.(interface{})
+		}
+	}
+	if item, ok := self.Spec["level"]; ok {
+		if value, ok := common.AliasCache.TryGetLevel(item.(string)); ok {
+			delete(self.Spec, "level")
+			self.Spec["levelId"] = value.Id.(interface{})
+		}
+	}
+	if item, ok := self.Spec["owner"]; ok {
+		if value, ok := common.AliasCache.TryGetTeam(item.(string)); ok {
+			delete(self.Spec, "owner")
+			self.Spec["ownerId"] = value.Id.(interface{})
+		}
+	}
+	if item, ok := self.Spec["filter"]; ok {
+		if value, ok := common.AliasCache.TryGetFilter(item.(string)); ok {
+			delete(self.Spec, "filter")
+			self.Spec["filterId"] = value.Id.(interface{})
+		}
+	}
+}
+
+func (self *CheckCreateType) AsCustomEventCreate() *opslevel.CheckCustomEventCreateInput {
+	if item, ok := self.Spec["integration"]; ok {
+		if value, ok := common.AliasCache.TryGetIntegration(item.(string)); ok {
+			delete(self.Spec, "integration")
+			self.Spec["integrationId"] = value.Id.(interface{})
+		}
+	}
+	self.Spec["resultMessage"] = self.Spec["message"]
+	payload := &opslevel.CheckCustomEventCreateInput{}
+	dataBytes, err := json.Marshal(self.Spec)
+	cobra.CheckErr(err)
+	json.Unmarshal(dataBytes, payload)
+	return payload
+}
+
+func readCheckCreateInput() (*CheckCreateType, error) {
+	readCreateConfigFile()
+	evt := &CheckCreateType{}
+	viper.Unmarshal(&evt)
+	if err := defaults.Set(evt); err != nil {
+		return nil, err
+	}
+	evt.resolveAliases()
+	return evt, nil
 }
