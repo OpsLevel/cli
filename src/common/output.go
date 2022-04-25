a2e59e0e4333fc956046501eba91cd784191ca94
Edgar Ochoa <edgar@opslevel.com>
2022-04-25 15:07:33 -0500

    Remove CSV writer

--- src/common/output.go
+++ src/common/output.go
@@ -16,29 +16,6 @@ func PrettyPrint(object interface{}) {
 	fmt.Println(string(bytes))
 }
 
-// TODO: Delete if we don't need it
-func NewCsvWriter(headers ...string) *tabwriter.Writer {
-	longestHeader := 0
-	var headerFormat strings.Builder
-	headersCasted := make([]interface{}, len(headers))
-	for i, header := range headers {
-		if i < len(headers)-1 {
-			headerFormat.WriteString("%s,")
-		} else {
-			headerFormat.WriteString("%s")
-		}
-		headersCasted[i] = header
-		headerLength := len(header)
-		if headerLength > longestHeader {
-			longestHeader = headerLength
-		}
-	}
-	headerFormat.WriteString("\n")
-	w := tabwriter.NewWriter(os.Stdout, longestHeader, longestHeader, 2, ' ', 0)
-	fmt.Fprintf(w, headerFormat.String(), headersCasted...)
-	return w
-}
-
 func NewTabWriter(headers ...string) *tabwriter.Writer {
 	longestHeader := 0
 	var headerFormat strings.Builder
