f8b3ba8670a3b6b456dad2abc8fe9aa7f76d825f
Matthew Brahms <matthew@opslevel.com>
2022-07-15 13:50:20 -0500

    consolidating APP_URL and API_URL

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -32,15 +32,13 @@ func Execute(v string, commit string) {
 func init() {
 	rootCmd.PersistentFlags().String("log-format", "TEXT", "overrides environment variable 'OPSLEVEL_LOG_FORMAT' (options [\"JSON\", \"TEXT\"])")
 	rootCmd.PersistentFlags().String("log-level", "INFO", "overrides environment variable 'OPSLEVEL_LOG_LEVEL' (options [\"ERROR\", \"WARN\", \"INFO\", \"DEBUG\"])")
-	rootCmd.PersistentFlags().String("api-url", "https://api.opslevel.com", "The OpsLevel API Url. Overrides environment variable 'OPSLEVEL_API_URL'")
-	rootCmd.PersistentFlags().String("app-url", "https://app.opslevel.com", "The OpsLevel App Url. Overrides environment variable 'OPSLEVEL_APP_URL'")
+	rootCmd.PersistentFlags().String("api-url", "https://app.opslevel.com", "The OpsLevel API Url. Overrides environment variable 'OPSLEVEL_API_URL'")
 	rootCmd.PersistentFlags().String("api-token", "", "The OpsLevel API Token. Overrides environment variable 'OPSLEVEL_API_TOKEN'")
 
 	viper.BindPFlags(rootCmd.PersistentFlags())
 	viper.BindEnv("log-format", "OPSLEVEL_LOG_FORMAT", "OL_LOG_FORMAT", "OL_LOGFORMAT")
 	viper.BindEnv("log-level", "OPSLEVEL_LOG_LEVEL", "OL_LOG_LEVEL", "OL_LOGLEVEL")
-	viper.BindEnv("api-url", "OPSLEVEL_API_URL", "OL_API_URL")
-	viper.BindEnv("app-url", "OPSLEVEL_APP_URL", "OL_APP_URL")
+	viper.BindEnv("api-url", "OPSLEVEL_API_URL", "OL_API_URL", "OPSLEVEL_APP_URL", "OL_APP_URL")
 	viper.BindEnv("api-token", "OPSLEVEL_API_TOKEN", "OL_API_TOKEN", "OL_APITOKEN")
 	cobra.OnInitialize(initConfig)
 }
@@ -76,7 +74,7 @@ func setupLogging() {
 
 func getClientRest() *resty.Client {
 	if _clientRest == nil {
-		_clientRest = opslevel.NewRestClient(opslevel.SetURL(viper.GetString("app-url")))
+		_clientRest = opslevel.NewRestClient(opslevel.SetURL(viper.GetString("api-url")))
 	}
 	return _clientRest
 }
