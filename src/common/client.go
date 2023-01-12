2fea43a81917ebdfb6b4c2fcee00f8277465306e
Matthew Brahms <matthew@opslevel.com>
2023-01-12 11:51:27 -0600

    adding `api-timeout` to cli

--- src/common/client.go
+++ src/common/client.go
@@ -2,6 +2,12 @@ package common
 
 import (
 	"fmt"
+<<<<<<< Updated upstream
+=======
+	"strings"
+	"time"
+
+>>>>>>> Stashed changes
 	"github.com/opslevel/opslevel-go/v2022"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -13,5 +19,21 @@ func NewGraphClient(version string) *opslevel.Client {
 		opslevel.SetURL(viper.GetString("api-url")),
 		opslevel.SetUserAgentExtra(fmt.Sprintf("cli-%s", version)))
 	cobra.CheckErr(client.Validate())
+	timeout := time.Second * time.Duration(viper.GetInt("api-timeout"))
+	client := opslevel.NewGQLClient(opslevel.SetAPIToken(viper.GetString("api-token")),
+		opslevel.SetURL(viper.GetString("api-url")),
+		opslevel.SetTimeout(timeout),
+		opslevel.SetUserAgentExtra(fmt.Sprintf("cli-%s", version)))
+
+	clientErr := client.Validate()
+	if clientErr != nil {
+		if strings.Contains(clientErr.Error(), "Please provide a valid OpsLevel API token") {
+			cobra.CheckErr(fmt.Errorf("%s via 'export OPSLEVEL_API_TOKEN=XXX' or '--api-token=XXX'", clientErr.Error()))
+		} else {
+			cobra.CheckErr(clientErr)
+		}
+	}
+	cobra.CheckErr(clientErr)
+
 	return client
 }
