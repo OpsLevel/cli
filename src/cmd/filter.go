7f71731b736d5daab825b3019af2a09081b166b7
Taimoor Ahmad <taimoor@opslevel.com>
2023-07-25 18:51:27 -0400

    create filter -f, update filter -f

--- src/cmd/filter.go
+++ src/cmd/filter.go
@@ -3,24 +3,41 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/creasty/defaults"
 	"github.com/opslevel/opslevel-go/v2023"
 
 	"github.com/opslevel/cli/common"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var createFilterCmd = &cobra.Command{
-	Use:        "filter NAME",
-	Short:      "Create a filter",
-	Long:       `Create a filter`,
-	Args:       cobra.ExactArgs(1),
-	ArgAliases: []string{"NAME"},
+	Use:   "filter",
+	Short: "Create a filter",
+	Long: `Create a filter
+
+cat << EOF | opslevel create filter -f -
+name: "Tier 1 Rails apps using RDS"
+connective: "and"
+predicates:
+  - key: "tier_index"
+    type: "equals"
+    value: "1"
+  - key: "filter_id"
+    type: "matches"
+    value: "Z2lkOi8vb3BzbGV2ZWwvRmlsdGVyLzEyNjQ"
+  - key: "tags"
+    keyData: "db"
+    type: "equals"
+    value: "rds"
+EOF`,
 	Run: func(cmd *cobra.Command, args []string) {
-		filter, err := getClientGQL().CreateFilter(opslevel.FilterCreateInput{
-			Name: args[0],
-		})
+		input, err := readFilterCreateInput()
 		cobra.CheckErr(err)
-		fmt.Println(filter.Id)
+		result, err := getClientGQL().CreateFilter(*input)
+		cobra.CheckErr(err)
+		fmt.Println(result.Id)
 	},
 }
 
@@ -59,6 +76,34 @@ var listFilterCmd = &cobra.Command{
 	},
 }
 
+var updateFilterCmd = &cobra.Command{
+	Use:   "filter",
+	Short: "Update a filter",
+	Long: `Update a filter
+
+cat << EOF | opslevel update filter -f -
+id: "Z2lkOi8vb3BzbGV2ZWwvRmlsdGVyLzIzNTk"
+name: "Apps using RDS or DynamoDB"
+connective: "or"
+predicates:
+  - key: "tags"
+    keyData: "db"
+    type: "equals"
+    value: "rds"
+  - key: "tags"
+    keyData: "db"
+    type: "equals"
+    value: "dynamo"
+EOF`,
+	Run: func(cmd *cobra.Command, args []string) {
+		input, err := readFilterUpdateInput()
+		cobra.CheckErr(err)
+		filter, err := getClientGQL().UpdateFilter(*input)
+		cobra.CheckErr(err)
+		common.JsonPrint(json.MarshalIndent(filter, "", "    "))
+	},
+}
+
 var deleteFilterCmd = &cobra.Command{
 	Use:        "filter ID",
 	Short:      "Delete a filter",
@@ -75,7 +120,28 @@ var deleteFilterCmd = &cobra.Command{
 
 func init() {
 	createCmd.AddCommand(createFilterCmd)
+	updateCmd.AddCommand(updateFilterCmd)
 	getCmd.AddCommand(getFilterCmd)
 	listCmd.AddCommand(listFilterCmd)
 	deleteCmd.AddCommand(deleteFilterCmd)
+}
+
+func readFilterCreateInput() (*opslevel.FilterCreateInput, error) {
+	readCreateConfigFile()
+	evt := &opslevel.FilterCreateInput{}
+	viper.Unmarshal(&evt)
+	if err := defaults.Set(evt); err != nil {
+		return nil, err
+	}
+	return evt, nil
+}
+
+func readFilterUpdateInput() (*opslevel.FilterUpdateInput, error) {
+	readUpdateConfigFile()
+	evt := &opslevel.FilterUpdateInput{}
+	viper.Unmarshal(&evt)
+	if err := defaults.Set(evt); err != nil {
+		return nil, err
+	}
+	return evt, nil
 }
