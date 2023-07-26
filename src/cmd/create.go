ce9564725bfef877324537f2ef119a1041f71882
Taimoor Ahmad <taimoor@opslevel.com>
2023-07-26 19:18:19 -0400

    combine create.go update.go into input.go

--- src/cmd/create.go
+++ src/cmd/create.go
@@ -1,14 +1,9 @@
 package cmd
 
 import (
-	"io"
-	"os"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-var createDataFile string
 
 var createCmd = &cobra.Command{
 	Use:   "create",
@@ -19,61 +14,6 @@ var createCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(createCmd)
 
-	createCmd.PersistentFlags().StringVarP(&createDataFile, "file", "f", "-", "File to read data from. If '.' then reads from './data.yaml'. Defaults to reading from stdin.")
+	createCmd.PersistentFlags().StringVarP(&dataFile, "file", "f", "-", "File to read data from. If '.' then reads from './data.yaml'. Defaults to reading from stdin.")
 	viper.BindPFlags(createCmd.Flags())
-}
-
-func hasStdin() bool {
-	stat, err := os.Stdin.Stat()
-	if err != nil {
-		return false
-	}
-	return stat.Size() > 0
-}
-
-func readCreateFile() ([]byte, error) {
-	if hasStdin() {
-		data, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			return nil, err
-		}
-		return data, nil
-	} else {
-		file, err := os.Open(createDataFile)
-		if err != nil {
-			return nil, err
-		}
-		defer file.Close()
-
-		data, err := io.ReadAll(file)
-		if err != nil {
-			return nil, err
-		}
-		return data, nil
-	}
-}
-
-func readCreateConfigFile() {
-	if createDataFile != "" {
-		if createDataFile == "-" {
-			viper.SetConfigType("yaml")
-			if hasStdin() {
-				viper.ReadConfig(os.Stdin)
-			}
-			return
-		} else if createDataFile == "." {
-			viper.SetConfigFile("./data.yaml")
-		} else {
-			viper.SetConfigFile(createDataFile)
-		}
-	} else {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		viper.SetConfigName("data")
-		viper.SetConfigType("yaml")
-		viper.AddConfigPath(".")
-		viper.AddConfigPath(home)
-	}
-	viper.ReadInConfig()
 }
--- src/cmd/update.go
+++ src/cmd/update.go
@@ -1,46 +1,19 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var updateDataFile string
-
 var updateCmd = &cobra.Command{
 	Use:   "update",
-	Short: "Update resources in OpsLevel",
-	Long:  "Update resources in OpsLevel",
+	Short: "Update resources or events from a file or stdin",
+	Long:  "Update resources or events from a file or stdin",
 }
 
 func init() {
 	rootCmd.AddCommand(updateCmd)
 
-	updateCmd.PersistentFlags().StringVarP(&updateDataFile, "file", "f", "-", "File to read update from. If '.' then reads from './data.yaml'. Defaults to reading from stdin.")
-	viper.BindPFlags(createCmd.Flags())
-}
-
-func readUpdateConfigFile() {
-	if updateDataFile != "" {
-		if updateDataFile == "-" {
-			viper.SetConfigType("yaml")
-			viper.ReadConfig(os.Stdin)
-			return
-		} else if updateDataFile == "." {
-			viper.SetConfigFile("./data.yaml")
-		} else {
-			viper.SetConfigFile(updateDataFile)
-		}
-	} else {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		viper.SetConfigName("data")
-		viper.SetConfigType("yaml")
-		viper.AddConfigPath(".")
-		viper.AddConfigPath(home)
-	}
-	viper.ReadInConfig()
+	updateCmd.PersistentFlags().StringVarP(&dataFile, "file", "f", "-", "File to read update from. If '.' then reads from './data.yaml'. Defaults to reading from stdin.")
+	viper.BindPFlags(updateCmd.Flags())
 }
