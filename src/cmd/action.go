7ddc45904b0127104ed1555e617bb4c65ca6e860
Taimoor Ahmad <taimoor@opslevel.com>
2023-08-11 14:08:43 -0400

    write out action.go trigger_definition.go

---
+++ src/cmd/action.go
@@ -0,0 +1,145 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/creasty/defaults"
+	"github.com/opslevel/opslevel-go/v2023"
+
+	"github.com/opslevel/cli/common"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// TODO: examples for create, update
+
+var actionType string
+
+var createActionCmd = &cobra.Command{
+	Use:   "action --type=$ACTION_TYPE",
+	Short: "Create an action",
+	Long:  "Create an action",
+	Example: `
+cat << EOF | opslevel create action --type=webhook -f -
+...
+EOF`,
+	Run: func(cmd *cobra.Command, args []string) {
+		switch actionType {
+		case "webhook":
+			input, err := readWebhookActionInput()
+			cobra.CheckErr(err)
+			result, err := getClientGQL().CreateWebhookAction(*input)
+			cobra.CheckErr(err)
+			fmt.Printf("created webhook action: %s\n", result.Id)
+		default:
+			err := errors.New("unknown action type: " + args[0])
+			cobra.CheckErr(err)
+		}
+	},
+}
+
+var getActionCmd = &cobra.Command{
+	Use:        "action ID",
+	Short:      "Get details about an action",
+	Long:       "Get details about an action",
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		action, err := getClientGQL().GetCustomAction(*opslevel.NewIdentifier(key))
+		cobra.CheckErr(err)
+		common.PrettyPrint(action)
+	},
+}
+
+var listActionCmd = &cobra.Command{
+	Use:     "action",
+	Aliases: []string{"actions"},
+	Short:   "List actions",
+	Long:    "List actions",
+	Run: func(cmd *cobra.Command, args []string) {
+		resp, err := getClientGQL().ListCustomActions(nil)
+		cobra.CheckErr(err)
+		list := resp.Nodes
+		if isJsonOutput() {
+			common.JsonPrint(json.MarshalIndent(list, "", "    "))
+		} else {
+			w := common.NewTabWriter("ID", "NAME")
+			for _, item := range list {
+				fmt.Fprintf(w, "%s\t%s\t\n", item.Id, item.Name)
+			}
+			w.Flush()
+		}
+	},
+}
+
+var updateActionCmd = &cobra.Command{
+	Use:   "action ID",
+	Short: "Update an action",
+	Long:  "Update an action",
+	Example: `
+cat << EOF | opslevel update action --type=webhook $ACTION_ID -f -
+...
+EOF`,
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		switch actionType {
+		case "webhook":
+			input, err := readWebhookActionInput()
+			cobra.CheckErr(err)
+			updateInput := &opslevel.CustomActionsWebhookActionUpdateInput{
+				Id:             *opslevel.NewID(key),
+				Name:           &input.Name,
+				Description:    input.Description,
+				LiquidTemplate: &input.LiquidTemplate,
+				WebhookURL:     &input.WebhookURL,
+				HTTPMethod:     input.HTTPMethod,
+				Headers:        &input.Headers,
+			}
+			action, err := getClientGQL().UpdateWebhookAction(*updateInput)
+			cobra.CheckErr(err)
+			common.JsonPrint(json.MarshalIndent(action, "", "    "))
+		default:
+			err := errors.New("unknown action type: " + actionType)
+			cobra.CheckErr(err)
+		}
+	},
+}
+
+var deleteActionCmd = &cobra.Command{
+	Use:        "action ID",
+	Short:      "Delete an action",
+	Long:       "Delete an action",
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		err := getClientGQL().DeleteWebhookAction(*opslevel.NewIdentifier(key))
+		cobra.CheckErr(err)
+		fmt.Printf("created webhook action: %s\n", key)
+	},
+}
+
+func init() {
+	createCmd.AddCommand(createActionCmd)
+	createCmd.Flags().StringVar(&actionType, "type", "webhook", "action type, default=webhook")
+	updateCmd.AddCommand(updateActionCmd)
+	updateCmd.Flags().StringVar(&actionType, "type", "webhook", "action type, default=webhook")
+	getCmd.AddCommand(getActionCmd)
+	listCmd.AddCommand(listActionCmd)
+	deleteCmd.AddCommand(deleteActionCmd)
+}
+
+func readWebhookActionInput() (*opslevel.CustomActionsWebhookActionCreateInput, error) {
+	readInputConfig()
+	evt := &opslevel.CustomActionsWebhookActionCreateInput{}
+	viper.Unmarshal(&evt)
+	if err := defaults.Set(evt); err != nil {
+		return nil, err
+	}
+	return evt, nil
+}
---
+++ src/cmd/trigger_definition.go
@@ -0,0 +1,132 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/creasty/defaults"
+	"github.com/opslevel/opslevel-go/v2023"
+
+	"github.com/opslevel/cli/common"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// TODO: examples for create, update
+
+var createTriggerDefinitionCmd = &cobra.Command{
+	Use:   "trigger-definition",
+	Short: "Create a trigger definition",
+	Long:  "Create a trigger definition",
+	Example: `
+cat << EOF | opslevel create trigger-definition -f -
+...
+EOF`,
+	Run: func(cmd *cobra.Command, args []string) {
+		input, err := readTriggerDefinitionInput()
+		cobra.CheckErr(err)
+		result, err := getClientGQL().CreateTriggerDefinition(*input)
+		cobra.CheckErr(err)
+		fmt.Printf("created trigger definition: %s\n", result.Id)
+	},
+}
+
+var getTriggerDefinitionCmd = &cobra.Command{
+	Use:        "trigger-definition ID",
+	Short:      "Get details about a trigger definition",
+	Long:       "Get details about a trigger definition",
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		triggerDefinition, err := getClientGQL().GetTriggerDefinition(*opslevel.NewIdentifier(key))
+		cobra.CheckErr(err)
+		common.PrettyPrint(triggerDefinition)
+	},
+}
+
+var listTriggerDefinitionCmd = &cobra.Command{
+	Use:     "trigger-definition",
+	Aliases: []string{"trigger-definitions"},
+	Short:   "List trigger definitions",
+	Long:    "List trigger definitions",
+	Run: func(cmd *cobra.Command, args []string) {
+		resp, err := getClientGQL().ListTriggerDefinitions(nil)
+		cobra.CheckErr(err)
+		list := resp.Nodes
+		if isJsonOutput() {
+			common.JsonPrint(json.MarshalIndent(list, "", "    "))
+		} else {
+			w := common.NewTabWriter("ID", "NAME")
+			for _, item := range list {
+				fmt.Fprintf(w, "%s\t%s\t\n", item.Id, item.Name)
+			}
+			w.Flush()
+		}
+	},
+}
+
+var updateTriggerDefinitionCmd = &cobra.Command{
+	Use:   "trigger-definition ID",
+	Short: "Update a trigger definition",
+	Long:  "Update a trigger definition",
+	Example: `
+cat << EOF | opslevel update trigger-definition $TRIGGER_ID -f -
+...
+EOF`,
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		input, err := readTriggerDefinitionInput()
+		cobra.CheckErr(err)
+		updateInput := &opslevel.CustomActionsTriggerDefinitionUpdateInput{
+			Id:                     *opslevel.NewID(key),
+			Name:                   &input.Name,
+			Description:            input.Description,
+			Owner:                  &input.Owner,
+			Action:                 &input.Action,
+			Filter:                 input.Filter,
+			ManualInputsDefinition: &input.ManualInputsDefinition,
+			Published:              input.Published,
+			AccessControl:          input.AccessControl,
+			ResponseTemplate:       &input.ResponseTemplate,
+			EntityType:             input.EntityType,
+		}
+		triggerDefinition, err := getClientGQL().UpdateTriggerDefinition(*updateInput)
+		cobra.CheckErr(err)
+		common.JsonPrint(json.MarshalIndent(triggerDefinition, "", "    "))
+	},
+}
+
+var deleteTriggerDefinitionCmd = &cobra.Command{
+	Use:        "trigger-definition ID",
+	Short:      "Delete a trigger definition",
+	Long:       "Delete a trigger definition",
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		key := args[0]
+		err := getClientGQL().DeleteTriggerDefinition(*opslevel.NewIdentifier(key))
+		cobra.CheckErr(err)
+		fmt.Printf("created trigger definition: %s\n", key)
+	},
+}
+
+func init() {
+	createCmd.AddCommand(createTriggerDefinitionCmd)
+	updateCmd.AddCommand(updateTriggerDefinitionCmd)
+	getCmd.AddCommand(getTriggerDefinitionCmd)
+	listCmd.AddCommand(listTriggerDefinitionCmd)
+	deleteCmd.AddCommand(deleteTriggerDefinitionCmd)
+}
+
+func readTriggerDefinitionInput() (*opslevel.CustomActionsTriggerDefinitionCreateInput, error) {
+	readInputConfig()
+	evt := &opslevel.CustomActionsTriggerDefinitionCreateInput{}
+	viper.Unmarshal(&evt)
+	if err := defaults.Set(evt); err != nil {
+		return nil, err
+	}
+	return evt, nil
+}
