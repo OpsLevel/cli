d751816c5fa8834246b7478ea32db04ad83dbfe2
David Bloss <david@opslevel.com>
2023-09-13 13:53:26 -0500

    print message when input coming from interactive tty

--- src/cmd/input.go
+++ src/cmd/input.go
@@ -1,32 +1,31 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var dataFile string
 
 func readInputConfig() {
-	if dataFile != "" {
-		if dataFile == "-" {
-			viper.SetConfigType("yaml")
-			viper.ReadConfig(os.Stdin)
-		} else if dataFile == "." {
-			viper.SetConfigFile("./data.yaml")
-		} else {
-			viper.SetConfigFile(dataFile)
+	switch dataFile {
+	case ".":
+		viper.SetConfigFile("./data.yaml")
+	case "-":
+		if isStdInFromTerminal() {
+			fmt.Println("Reading input directly from command line...")
 		}
-	} else {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		viper.SetConfigName("data")
 		viper.SetConfigType("yaml")
-		viper.AddConfigPath(".")
-		viper.AddConfigPath(home)
+		viper.ReadConfig(os.Stdin)
+	default:
+		viper.SetConfigFile(dataFile)
 	}
 	viper.ReadInConfig()
+}
+
+func isStdInFromTerminal() bool {
+	fi, _ := os.Stdin.Stat()
+	return fi.Mode()&os.ModeCharDevice != 0
 }
