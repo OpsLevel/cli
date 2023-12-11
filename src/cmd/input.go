4bd598d92110200ea5d5579fe4ab580a86dc7715
David Bloss <david@opslevel.com>
2023-12-11 10:44:26 -0600

    have readResourceInput read YAML apart from viper config

--- src/cmd/input.go
+++ src/cmd/input.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
+	"bytes"
 	"os"
 
 	"github.com/rs/zerolog/log"
 
-	"github.com/creasty/defaults"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
 
 var dataFile string
@@ -30,13 +31,31 @@ func readInputConfig() {
 }
 
 func readResourceInput[T any]() (*T, error) {
-	readInputConfig()
-	var evt T
-	viper.Unmarshal(&evt)
-	if err := defaults.Set(&evt); err != nil {
+	var err error
+	var resource T
+	var yamlData []byte
+
+	switch dataFile {
+	case ".":
+		yamlData, err = os.ReadFile("./data.yaml")
+	case "-":
+		if isStdInFromTerminal() {
+			log.Info().Msg("Reading input directly from command line... Press CTRL+D to stop typing")
+		}
+		buf := bytes.Buffer{}
+		_, err = buf.ReadFrom(os.Stdin)
+		yamlData = buf.Bytes()
+	default:
+		yamlData, err = os.ReadFile(dataFile)
+	}
+	if err != nil {
 		return nil, err
 	}
-	return &evt, nil
+
+	if err := yaml.Unmarshal(yamlData, &resource); err != nil {
+		return nil, err
+	}
+	return &resource, nil
 }
 
 func isStdInFromTerminal() bool {
