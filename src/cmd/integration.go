b5052a51169c1902caa8e5fa80fd6e793a9aa6f6
Kyle <rocktavious@icloud.com>
2024-04-04 09:21:54 -0500

    Add missing integration commands (#270)
    * Add missing integration commands

--- src/cmd/integration.go
+++ src/cmd/integration.go
@@ -4,11 +4,71 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/opslevel/opslevel-go/v2024"
-
+	"github.com/mitchellh/mapstructure"
 	"github.com/opslevel/cli/common"
+	"github.com/opslevel/opslevel-go/v2024"
 	"github.com/spf13/cobra"
 )
+
+type IntegrationType string
+
+const (
+	IntegrationTypeAWS IntegrationType = "aws"
+)
+
+var IntegrationConfigCurrentVersion = "1"
+
+type IntegrationInputType struct {
+	Version string `yaml:"version"`
+	Kind    IntegrationType
+	Spec    map[string]interface{}
+}
+
+type IntegrationInput interface {
+	opslevel.AWSIntegrationInput
+}
+
+func readIntegrationInput[T IntegrationInput]() (T, error) {
+	var output T
+	input, err := readResourceInput[IntegrationInputType]()
+	if err != nil {
+		return output, err
+	}
+	if input.Version != CheckConfigCurrentVersion {
+		return output, fmt.Errorf("supported config version is '%s' but found '%s'",
+			IntegrationConfigCurrentVersion, input.Version)
+	}
+	// TODO: need to use input.Kind and a switch statement - but currently we only support AWS
+	if err := mapstructure.Decode(input.Spec, &output); err != nil {
+		return output, err
+	}
+	return output, nil
+}
+
+var createIntegrationCmd = &cobra.Command{
+	Use:     "integration",
+	Aliases: []string{"integrations", "int", "ints"},
+	Short:   "Create an integration",
+	Long:    `Create an integration`,
+	Example: `cat << EOF | opslevel create integration -f -
+version: 1
+kind: aws
+spec:
+  name: "Prod"
+  iamRole: "arn:aws:iam::XXXXX:role/opslevel-integration"
+  externalId: "XXXXXX"
+  ownershipTagOverrides: true
+  ownershipTagKeys: ["owner","service","app"]
+EOF
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		input, err := readIntegrationInput[opslevel.AWSIntegrationInput]()
+		cobra.CheckErr(err)
+		resp, err := getClientGQL().CreateIntegrationAWS(input)
+		cobra.CheckErr(err)
+		fmt.Printf("Created Integration '%s' with id '%s'\n", resp.Name, resp.Id)
+	},
+}
 
 var getIntegrationCmd = &cobra.Command{
 	Use:        "integration ID",
@@ -46,7 +106,32 @@ var listIntegrationCmd = &cobra.Command{
 	},
 }
 
+var updateIntegrationCmd = &cobra.Command{
+	Use:     "integration ID",
+	Aliases: []string{"int"},
+	Short:   "Update an integration",
+	Long:    `Update an integration`,
+	Example: `cat << EOF | opslevel update integration XXXXXXXX -f -
+version: 1
+kind: aws
+spec:
+  ownershipTagOverrides: true
+  ownershipTagKeys: ["owner","service","app"]
+EOF`,
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"ID"},
+	Run: func(cmd *cobra.Command, args []string) {
+		input, err := readIntegrationInput[opslevel.AWSIntegrationInput]()
+		cobra.CheckErr(err)
+		resp, err := getClientGQL().UpdateIntegrationAWS(args[0], input)
+		cobra.CheckErr(err)
+		fmt.Printf("Updated Integration '%s' with id '%s'\n", resp.Name, resp.Id)
+	},
+}
+
 func init() {
+	createCmd.AddCommand(createIntegrationCmd)
 	getCmd.AddCommand(getIntegrationCmd)
 	listCmd.AddCommand(listIntegrationCmd)
+	updateCmd.AddCommand(updateIntegrationCmd)
 }
