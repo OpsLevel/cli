6267502a6c71223586a2d48c4bc15aa99c02a416
Kyle Rockman <kyle@opslevel.com>
2025-04-14 09:42:28 -0500

    Blockout bigger MCP server

---
+++ src/cmd/mcp.go
@@ -0,0 +1,147 @@
+package cmd
+
+import (
+	"encoding/json"
+	mcp_golang "github.com/metoro-io/mcp-golang"
+	"github.com/metoro-io/mcp-golang/transport/stdio"
+	"github.com/spf13/cobra"
+)
+
+type NullArguments struct{}
+
+type LightweightComponent struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Owner string `json:"owner"`
+	URL   string `json:"url"`
+}
+
+var mcpCmd = &cobra.Command{
+	Use:   "mcp",
+	Short: "MCP Server",
+	Long:  "MCP Server",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		done := make(chan struct{})
+
+		//transport := http.NewHTTPTransport("/mcp")
+		//transport.WithAddr(":8080")
+		//server := mcp_golang.NewServer(transport)
+		server := mcp_golang.NewServer(stdio.NewStdioServerTransport())
+
+		// Register Teams
+		if err := server.RegisterTool("teams", "Get all the team names, identifiers and metadata for the opslevel account.  Teams are owners of other objects in opslevel. Only use this if you need to search all teams.", func(args NullArguments) (*mcp_golang.ToolResponse, error) {
+			client := getClientGQL()
+			resp, err := client.ListTeams(nil)
+			if err != nil {
+				return nil, err
+			}
+			data, err := json.Marshal(resp.Nodes)
+			if err != nil {
+				return nil, err
+			}
+			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(string(data))), nil
+		}); err != nil {
+			panic(err)
+		}
+
+		// Register Users
+		if err := server.RegisterTool("users", "Get all the user names, e-mail addresses and metadata for the opslevel account.  Users are the people in opslevel. Only use this if you need to search all users.", func(args NullArguments) (*mcp_golang.ToolResponse, error) {
+			client := getClientGQL()
+			resp, err := client.ListUsers(nil)
+			if err != nil {
+				return nil, err
+			}
+			data, err := json.Marshal(resp.Nodes)
+			if err != nil {
+				return nil, err
+			}
+			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(string(data))), nil
+		}); err != nil {
+			panic(err)
+		}
+
+		// Register Actions
+		if err := server.RegisterTool("actions", "Get all the information about actions the user can run in the opslevel account", func(args NullArguments) (*mcp_golang.ToolResponse, error) {
+			client := getClientGQL()
+			resp, err := client.ListTriggerDefinitions(nil)
+			if err != nil {
+				return nil, err
+			}
+			data, err := json.Marshal(resp.Nodes)
+			if err != nil {
+				return nil, err
+			}
+			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(string(data))), nil
+		}); err != nil {
+			panic(err)
+		}
+
+		// Register Filters
+		if err := server.RegisterTool("filters", "Get all the rubric filter names and which predicates they have for the opslevel account", func(args NullArguments) (*mcp_golang.ToolResponse, error) {
+			client := getClientGQL()
+			resp, err := client.ListFilters(nil)
+			if err != nil {
+				return nil, err
+			}
+			data, err := json.Marshal(resp.Nodes)
+			if err != nil {
+				return nil, err
+			}
+			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(string(data))), nil
+		}); err != nil {
+			panic(err)
+		}
+
+		// Register Components
+		if err := server.RegisterTool("components", "Get all the components in the opslevel account.  Components are objects in opslevel that represent things like apis, libraries, services, frontends, backends, etc.", func(args NullArguments) (*mcp_golang.ToolResponse, error) {
+			client := getClientGQL()
+			resp, err := client.ListServices(nil)
+			if err != nil {
+				return nil, err
+			}
+			var components []LightweightComponent
+			for _, node := range resp.Nodes {
+				components = append(components, LightweightComponent{
+					Id:    string(node.Id),
+					Name:  node.Name,
+					Owner: node.Owner.Alias,
+					URL:   node.HtmlURL,
+				})
+			}
+			data, err := json.Marshal(resp.Nodes)
+			if err != nil {
+				return nil, err
+			}
+			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(string(data))), nil
+		}); err != nil {
+			panic(err)
+		}
+
+		// Register Infra
+		if err := server.RegisterTool("infrastructure", "Get all the infrastructure in the opslevel account.  Infrastructure are objects in opslevel that represent cloud provider resources like vpc, databases, caches, networks, vms, etc.", func(args NullArguments) (*mcp_golang.ToolResponse, error) {
+			client := getClientGQL()
+			resp, err := client.ListInfrastructure(nil)
+			if err != nil {
+				return nil, err
+			}
+			data, err := json.Marshal(resp.Nodes)
+			if err != nil {
+				return nil, err
+			}
+			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(string(data))), nil
+		}); err != nil {
+			panic(err)
+		}
+
+		if err := server.Serve(); err != nil {
+			panic(err)
+		}
+		<-done
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(mcpCmd)
+}
--- src/go.mod
+++ src/go.mod
@@ -31,7 +31,9 @@ require (
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/ProtonMail/go-crypto v1.1.5 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/cloudflare/circl v1.6.0 // indirect
@@ -40,6 +42,8 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.6.2 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
@@ -50,6 +54,7 @@ require (
 	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
@@ -58,12 +63,18 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hasura/go-graphql-client v0.13.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/invopop/jsonschema v0.12.0 // indirect
 	github.com/itchyny/timefmt-go v0.1.6 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/metoro-io/mcp-golang v0.9.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opslevel/moredefaults v0.0.0-20240529152742-17d1318a3c12 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
@@ -83,6 +94,12 @@ require (
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tchap/go-patricia/v2 v2.3.1 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
